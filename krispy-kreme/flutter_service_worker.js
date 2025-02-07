042d42ff35b7a566af9c4c3e857715fe3d8cffb1
edisonlin31 <edisonjnz31@gmail.com>
2025-02-07 14:50:38 +0700

    update

--- krispy-kreme/flutter_service_worker.js
+++ krispy-kreme/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "218dcf1eb461f6732ec328a15b609063",
-"version.json": "7328e911edfe60de755b6df346a8d120",
+const RESOURCES = {"flutter_bootstrap.js": "8db582a312e4694edaa08402c4ee7731",
+"version.json": "08bae8d6cfd1f82ec42b9cbfd17ea489",
 "index.html": "742a7c8c15b1ee72d8eccbd046705cc7",
 "/": "742a7c8c15b1ee72d8eccbd046705cc7",
-"main.dart.js": "4990ab841797e6abf8dd61cc3a268aaa",
+"main.dart.js": "e76db6589ab055fb9bd3068ea33180c7",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "622bbbae97b2c62d5f077ecd82871718",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
