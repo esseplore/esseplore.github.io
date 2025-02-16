fa63fc909eaf111fa5336b870d3c14ccb3ec46cb
edisonlin31 <edisonjnz31@gmail.com>
2025-02-16 23:11:44 +0700

    update

--- essecater/flutter_service_worker.js
+++ essecater/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "87a5137c42a990f28f7cdf500b876ff2",
+const RESOURCES = {"flutter_bootstrap.js": "2cd6c10d3d593a486f4f4f092e017b66",
 "version.json": "a55166145d2dc3df5c8615a76d481dfe",
 "index.html": "9c85c66f14f0f5cb2817124fb003c88e",
 "/": "9c85c66f14f0f5cb2817124fb003c88e",
-"main.dart.js": "d09ba0fd157e3041c39d5e5f61b1c413",
+"main.dart.js": "0150ab8706131ca9a5d22c11035119dc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
